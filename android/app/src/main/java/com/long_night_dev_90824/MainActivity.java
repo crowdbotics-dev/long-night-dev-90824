765fcb29e9e265015f350569176501502dcdb508
Crowdbotics <engineering@crowdbotics.com>
2023-06-12 14:31:51 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/long_night_dev_90824/MainActivity.java
@@ -0,0 +1,15 @@
+package com.long_night_dev_90824;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "long_night_dev_90824";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "long-night-dev-90824",
+  url: "https://long-night-dev-90824.botics.co",
+  api: "https://long-night-dev-90824.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
